94db7f7af6cc1ee5cc8a1f7664dd6204bb570ad8
ThePowerdinoDeluxe990 <toniservera990@gmail.com>
2024-09-10 14:14:43 +0200

    Added toast, when the download is finished

--- app/src/main/java/com/powerdino/splatoonwallpapers/downloader/DownloadCompleteReceiver.kt
+++ app/src/main/java/com/powerdino/splatoonwallpapers/downloader/DownloadCompleteReceiver.kt
@@ -5,6 +5,7 @@ import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import androidx.work.impl.utils.ForceStopRunnable.BroadcastReceiver
 
 @SuppressLint("RestrictedApi")
@@ -13,6 +14,7 @@ class DownloadCompleteReceiver:BroadcastReceiver() {
         if(intent?.action == "android.intent.action.DOWNLOAD_COMPLETE"){
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1L)
             if(id != -1L){
+                Toast.makeText(context, "Download finished!", Toast.LENGTH_SHORT).show()
                 Log.e("Info","Download with ID $id finished!")
             }
         }
